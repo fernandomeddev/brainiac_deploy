c1ee29475d66ea36e27fa6ec21a96d67c0de61f4
Fernando Medeiros <fernandohenrique520@live.com>
2023-01-29 22:35:23 -0300

    nova rota update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
     return response.json({ message: 'Server Is Up'});
+});
+
+app.get('/updates', (request, response) => {
+    return response.json({ message: 'Server is Updated!'});
 });
 
 app.post('/test', (request, response) => {
