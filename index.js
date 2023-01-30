97590abdbdcc9b40caa1b6a1f16ae75baf963b86
Fernando Medeiros <fernandohenrique520@live.com>
2023-01-29 21:56:25 -0300

    rotas de test

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.send(200);
-})
+    return response.json({ message: 'Server Is Up'});
+});
+
+app.post('/test', (request, response) => {
+    const { name, date } = request.body;
+    return response.json({ name, date })
+});
 
 app.listen(3333);
