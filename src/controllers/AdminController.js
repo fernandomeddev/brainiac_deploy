b619db01b79efd26cd1c0516a03b993f7a1a05b8
Fernando Medeiros <fernandohenrique520@live.com>
2023-02-03 13:46:59 -0300

    refactor controlls

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { ObjectId } = require('mongoose');
 const authRole = require('../config/auth');
 const IndividualModel = require('../models/IndividualModel');
 
@@ -16,8 +15,6 @@ router.put('/individual_updade/:individual_id', async (request, response) => {
             ...request.body
         }
 
-        console.log(data )
-
         const updatedIndividual = await IndividualModel.findByIdAndUpdate(individualId, { isAdmin: true });
         if (!updatedIndividual) return response.json({ message: 'updated failed' });
 
@@ -25,7 +22,7 @@ router.put('/individual_updade/:individual_id', async (request, response) => {
 
         return response.status(200).json({ responseData: updatedIndividual });
     } catch (error) {
-        return response.send(error);
+        if(error) return response.status(500).json({ message: 'sorry... try again later'});
     }
 });
 
@@ -34,10 +31,11 @@ router.get('/list_individual', async (request, response) => {
     try {
         const individual = await IndividualModel.find();
         if (!individual) return response.status(401).send({ error: 'list is empty' });
-        
+
         return response.send({ individual });
+
     } catch (error) {
-        return response.status(400).send({ error: 'Error an loading accounts' });
+        if(error) return response.status(500).json({ message: 'sorry... try again later'});
     };
 });
 
@@ -47,33 +45,29 @@ router.get('/info_individual/:individual_id', async (request, response) => {
         const { individual_id: individualId } = request.params;
         const individual = await IndividualModel.findById(individualId);
 
-        if(!individual){
-            return response.status(404).send('individual not found')
-        }
+        if(!individual) return response.status(404).send('individual not found')
         
         return response.json({ data: individual });
 
     } catch (error) {
-        return response.send(error)
+        if(error) return response.status(500).json({ message: 'sorry... try again later'});
     }
 });
 
 router.delete('/info_individual/:individual_id', async (request, response) => {
     try {
         const { individual_id: individualId } = request.params;
+
         const individual = await IndividualModel.findById(individualId);
         if (!individual) return response.status(401).json({ message: 'individual not found' });
 
         const individualDeleted = await IndividualModel.findByIdAndDelete(individualId);
-
-        if(!individualDeleted){
-            return response.status(404).send('individual not be deleted');
-        }
+        if(!individualDeleted) return response.status(404).send('individual not be deleted');
 
         return response.json({ data: individual, message: 'individual deleted!' });
 
     } catch (error) {
-        return response.send(error)
+        if(error) return response.status(500).json({ message: 'sorry... try again later'});
     }
 });
 
--- src/controllers/IndividualController.js
+++ src/controllers/IndividualController.js
@@ -1,10 +1,10 @@
 require('dotenv');
-
 const express = require("express");
+const router = express.Router();
 const authSecret = process.env.SECRET;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const router = express.Router();
+
 const IndividualModel = require('../models/IndividualModel');
 
 function generateToken(params = {}) {
@@ -13,52 +13,50 @@ function generateToken(params = {}) {
     });
 };
 
-
 router.post('/signup', async (request, response) => {
     try {
         const { name, email, password, confirm_password: confirmPassword, isAdmin="false" } = request.body;
 
-            if (!name) return response.json({ message: 'name is required!' });
-            if (!email) return response.json({ message: 'name is required!' });
-            if (password !== confirmPassword) return response.json({message: 'passwords do not match'});
-            
-            const individual = await IndividualModel.findOne({ email: email });
-            if (individual) return response.json({ message: 'user already exists!' });
+        if (!name) return response.json({ message: 'name is required!' });
+        if (!email) return response.json({ message: 'name is required!' });
+        if (password !== confirmPassword) return response.json({message: 'passwords do not match'});
+        
+        const individual = await IndividualModel.findOne({ email: email });
+        if (individual) return response.json({ message: 'user already exists!' });
 
-            const data = {
-               ...request.body
-            };
+        const data = {
+            ...request.body
+        };
 
-            const newIndividual = await IndividualModel.create(data);
+        const newIndividual = await IndividualModel.create(data);
 
-            return response.status(200).json({ data: newIndividual });
+        return response.status(200).json({ data: newIndividual });
     } catch (error) {
-        return response.send(error);
+        if(error) return response.status(500).json({ message: 'sorry... try again later'});
     }
 });
 
-
 router.post('/signin', async (request, response) => {
-    const {email, password } = request.body;
+    try {
+        const {email, password } = request.body;
+        const individual = await IndividualModel.findOne({ email }).select('+password')
 
-    const user = await IndividualModel.findOne({ email }).select('+password')
+        if (!individual) return response.status(400).send({ error: 'User not found!'})  
+        if(!await bcrypt.compare(password, individual.password)) return response.status(400).send({erro: 'Invalid password'})
+            
+        individual.password = undefined;
 
-    if (!user) 
-        return res.status(400).send({ error: 'User not found!'})
+        const token = jwt.sign({ id: individual.id }, authSecret, {
+            expiresIn: 86400,
+        });
 
-    if(!await bcrypt.compare(password, user.password))
-        return res.status(400).send({erro: 'Invalid password'})
-
-    user.password = undefined;
-
-    const token = jwt.sign({ id: user.id }, authSecret, {
-        expiresIn: 86400,
-    });
-
-    response.send({ 
-        user, 
-        token: generateToken({ id: user.id }),
-    });
+        response.send({ 
+            individual, 
+            token: generateToken({ id: individual.id }),
+        });
+    } catch (error) {
+        if(error) return response.status(500).json({ message: 'sorry... try again later'});  
+    }
 });
 
 module.exports = app => app.use('/auth', router);
