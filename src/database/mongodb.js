c64c7392ae638bc6e7f13443149d619c4eb5ea21
Fernando Medeiros <fernandohenrique520@live.com>
2023-02-14 18:34:12 -0300

    first commit

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,16 +1,18 @@
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
 const mongoose = require('mongoose');
+let serverConection = false;
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.oddb3.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true })
     .then(() => {
-        console.log('connected with mongoDB')
+        serverConection = true;
+        console.log('connected!')
     })
     .catch(e => {
-        const msg = 'MongoDB conection Error!'
+        const error = { status: 500, message: 'não foi possivel se conectar aos nossos servidores, tente novamente mais tarde' }
 
-        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m' + e)
-        //connection <monitor> to 54.232.44.65:27017 closed - fix it updating your IP:Adress in network section on atlas, in your cluster.
+        console.log('\x1b[41m%s\x1b[37m', error.message, '\x1b[0m' + e)
+        // connection updating your IP:Adress in network section on atlas, in your cluster.
     })
 
 module.exports = mongoose;
